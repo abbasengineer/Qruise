c8ba398f91c66c7e4f2ef0a5d964512b405b9d8d
Abbas Engineer <abbster52@hotmail.com>
2024-05-25 16:15:25 -0700

    added about me and autofill

--- public/script.js
+++ public/script.js
@@ -1,11 +1,16 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 37.397, lng: -122.447},
+        center: {lat: 37.3977, lng: -122.4477},
         zoom: 8
     });
     var directionsService = new google.maps.DirectionsService();
     var directionsRenderer = new google.maps.DirectionsRenderer();
     directionsRenderer.setMap(map);
+
+    // Initialize the Autocomplete functionality for the input fields
+    startAutocomplete = new google.maps.places.Autocomplete(document.getElementById('start'));
+    endAutocomplete = new google.maps.places.Autocomplete(document.getElementById('end'));
+
 
     document.getElementById('routeForm').addEventListener('submit', function(e) {
         e.preventDefault();
